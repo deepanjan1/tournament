d437f878b7abec8a4901ef636586b1734efa5632
deepanjan1 <deepanjan.de@gmail.com>
2016-12-27 17:25:44 -0500

    made review changes

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -7,117 +7,126 @@ import psycopg2
 
 
 def connect():
-	"""Connect to the PostgreSQL database.  Returns a database connection."""
-	return psycopg2.connect("dbname=tournament")
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
-	"""Remove all the match records from the database."""
-	DB = connect()
-	c = DB.cursor()
-	# c.execute("DELETE FROM match")
-	c.execute("UPDATE player SET wins=0,losses=0")
-	DB.commit()
-	DB.close()
+    """Remove all the match records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM match")
+    DB.commit()
+    DB.close()
+
 
 def deletePlayers():
-	"""Remove all the player records from the database."""
-	DB = connect()
-	c = DB.cursor()
-	c.execute("DELETE FROM player")
-	DB.commit()
-	DB.close()
+    """Remove all the player records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM player")
+    DB.commit()
+    DB.close()
+
 
 def countPlayers():
-	"""Returns the number of players currently registered."""
-	DB = connect()
-	c = DB.cursor()
-	c.execute("SELECT count(*) AS number FROM player")
-	players = c.fetchall()
-	DB.close()
-	return int(players[0][0])
+    """Returns the number of players currently registered."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT count(*) AS number FROM player")
+    players = c.fetchall()
+    DB.close()
+    return int(players[0][0])
 
 
 def registerPlayer(name):
-	"""Adds a player to the tournament database.
-  
-	The database assigns a unique serial id number for the player.  (This
-	should be handled by your SQL database schema, not in your Python code.)
-  
-	Args:
-	  name: the player's full name (need not be unique).
-	"""
-	DB = connect()
-	c = DB.cursor()
-	c.execute("INSERT INTO player (name) VALUES ('%s')" % name.replace("'", "\\"))
-	DB.commit()
-	DB.close()
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    DB = connect()
+    c = DB.cursor()
+    data = (name, )
+    c.execute("INSERT INTO player (name) VALUES (%s)", data)
+    DB.commit()
+    DB.close()
+
 
 def playerStandings():
-	"""Returns a list of the players and their win records, sorted by wins.
+    """Returns a list of the players and their win records, sorted by wins.
 
-	The first entry in the list should be the player in first place, or a player
-	tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
 
-	Returns:
-	  A list of tuples, each of which contains (id, name, wins, matches):
-		id: the player's unique id (assigned by the database)
-		name: the player's full name (as registered)
-		wins: the number of matches the player has won
-		matches: the number of matches the player has played
-	"""
-	DB = connect()
-	c = DB.cursor()
-	c.execute("SELECT userid,name, COALESCE(wins,0), COALESCE(wins+losses,0) FROM player ORDER BY wins DESC")
-	player_wins = c.fetchall()
-	DB.commit()
-	DB.close()
-	return player_wins
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+            id: the player's unique id (assigned by the database)
+            name: the player's full name (as registered)
+            wins: the number of matches the player has won
+            matches: the number of matches the player has played
+    """
+    DB = connect()
+    c = DB.cursor()
+    c.execute(
+        "SELECT userid,name,wins,COALESCE(wins+losses,0) FROM standings ORDER BY wins DESC")
+    player_wins = c.fetchall()
+    DB.commit()
+    DB.close()
+    return player_wins
+
 
 def reportMatch(winner, loser):
-	"""Records the outcome of a single match between two players.
+    """Records the outcome of a single match between two players.
 
-	Args:
-	  winner:  the id number of the player who won
-	  loser:  the id number of the player who lost
-	"""
-	DB = connect()
-	c = DB.cursor()
-	# c.execute("INSERT INTO match (winner, loser) VALUES (%s,%s)" % (winner, loser))
-	c.execute("UPDATE player SET wins = wins+1 WHERE userid = %d" % winner)
-	c.execute("UPDATE player SET losses = losses+1 WHERE userid = %d" % loser)
-	DB.commit()
-	DB.close()
- 
- 
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    DB = connect()
+    c = DB.cursor()
+    win = (winner, )
+    loss = (loser, )
+    c.execute("INSERT INTO match (winner, loser) VALUES (%s,%s)", (win, loss))
+    DB.commit()
+    DB.close()
+
+
 def swissPairings():
-	"""Returns a list of pairs of players for the next round of a match.
-  
-	Assuming that there are an even number of players registered, each player
-	appears exactly once in the pairings.  Each player is paired with another
-	player with an equal or nearly-equal win record, that is, a player adjacent
-	to him or her in the standings.
-  
-	Returns:
-	  A list of tuples, each of which contains (id1, name1, id2, name2)
-		id1: the first player's unique id
-		name1: the first player's name
-		id2: the second player's unique id
-		name2: the second player's name
-	"""
-	DB = connect()
-	c = DB.cursor()
-	c.execute("SELECT userid,name FROM player ORDER BY wins DESC")
-	player_wins = c.fetchall()
-	c.execute("SELECT count(*) AS num FROM player")
-	num = int(c.fetchall()[0][0])
-	matches = []
-	i = 0
-	while i < num:
-		matches.append((player_wins[i][0],player_wins[i][1],player_wins[i+1][0],player_wins[i+1][1]))
-		i+=2
-	DB.commit()
-	DB.close()
-	return matches
+    """Returns a list of pairs of players for the next round of a match.
 
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
 
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+            id1: the first player's unique id
+            name1: the first player's name
+            id2: the second player's unique id
+            name2: the second player's name
+    """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT userid,name FROM standings ORDER BY wins DESC")
+    player_wins = c.fetchall()
+    c.execute("SELECT count(*) AS num FROM standings")
+    num = int(c.fetchall()[0][0])
+    matches = []
+    i = 0
+    while i < num:
+        matches.append(
+            (player_wins[i][0],
+             player_wins[i][1],
+             player_wins[
+                i + 1][0],
+                player_wins[
+                i + 1][1]))
+        i += 2
+    DB.commit()
+    DB.close()
+    return matches
--- tournament.sql
+++ tournament.sql
@@ -6,11 +6,23 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- CREATE TABLE player (
+-- 	userid SERIAL PRIMARY KEY NOT NULL,
+-- 	name TEXT NOT NULL,	
+-- 	wins INT DEFAULT 0,
+-- 	losses INT DEFAULT 0);
+
+
+-- Create db and establish schema
+
+DROP DATABASE tournament;
+CREATE DATABASE tournament;
+\c tournament;
+
+
 CREATE TABLE player (
-	userid SERIAL PRIMARY KEY NOT NULL,
-	name TEXT NOT NULL,	
-	wins INT DEFAULT 0,
-	losses INT DEFAULT 0);
+	userid SERIAL PRIMARY KEY,
+	name TEXT NOT NULL);
 
 
 --ARCHIVED TABLES
@@ -20,11 +32,37 @@ CREATE TABLE player (
 -- 	wins INT DEFAULT 0,
 -- 	losses INT DEFAULT 0);
 
--- CREATE TABLE match (
--- 	winner INT REFERENCES player(userid),
--- 	loser INT REFERENCES player(userid));
+CREATE TABLE match (
+	matchid SERIAL PRIMARY KEY,
+	winner INT,
+	loser INT,
+	FOREIGN KEY (winner) REFERENCES player(userid),
+	FOREIGN KEY (loser) REFERENCES player(userid));
 
--- CREATE VIEW standings AS
--- 	SELECT userid,name,wins,(wins+losses) AS matches 
--- 	FROM player 
--- 	ORDER BY wins DESC;
+CREATE VIEW wins AS
+	SELECT winner AS userid, count(winner) as wins 
+	FROM match 
+	GROUP BY userid;
+
+CREATE VIEW losses AS
+	SELECT loser AS userid, count(loser) as losses 
+	FROM match 
+	GROUP BY userid;
+
+CREATE VIEW standings AS
+	SELECT player.userid, player.name, COALESCE(wins.wins,0) as wins, COALESCE(losses.losses,0) as losses 
+	FROM 
+		player 
+		LEFT JOIN wins ON player.userid=wins.userid
+		LEFT JOIN losses ON player.userid=losses.userid
+	ORDER BY wins.wins;
+
+INSERT INTO player(name) VALUES('Deep');
+INSERT INTO player(name) VALUES('Harini');
+INSERT INTO player(name) VALUES('Minty');
+INSERT INTO player(name) VALUES('Azeem');
+
+INSERT INTO match(winner,loser) VALUES(4, 1);
+INSERT INTO match(winner,loser) VALUES(3, 2);
+INSERT INTO match(winner,loser) VALUES(2, 1);
+INSERT INTO match(winner,loser) VALUES(4, 3);
